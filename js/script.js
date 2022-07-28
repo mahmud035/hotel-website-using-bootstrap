acae13e8124450b3129704c1ecd8f526cff07574
mahmud035 <mahamudulhasan4148@gmail.com>
2022-07-29 03:52:03 +0600

    added swiper slider to banner section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// Header Scroll
+
+// Nav Hide
+
+// Swiper Slider
+var swiper = new Swiper('.mySwiper', {
+  direction: 'vertical',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3500,
+  },
+});
+
+// Counter Design
+
+// Our Partners
