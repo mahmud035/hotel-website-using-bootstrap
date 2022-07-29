9414436ea8d02625f6bd00e04c06e295f0afcfff
mahmud035 <mahamudulhasan4148@gmail.com>
2022-07-29 06:10:41 +0600

    added booking area css and navbar scroll function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 'use strict';
 
 // Header Scroll
+let nav = document.querySelector('.navbar');
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 50) {
+    nav.classList.add('header-scrolled');
+  } else {
+    nav.classList.remove('header-scrolled');
+  }
+};
 
 // Nav Hide
 
